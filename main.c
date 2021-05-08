2f222990927223ce382d7314757c42031e7d77d6
Hassan Juma <okothhassanjuma@gmail.com>
2021-05-08 09:56:35 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("     /|\n");
+    printf("    / |\n");
+    printf("   /  |\n");
+    printf("  /   |\n");
+    printf(" /    |\n");
+    printf("/_____|\n");
+    return 0;
+}
